f1ba5d22408530c49829c0cf27df15f608f9eea7
yourmomyourdad <103446323+yourmomyourdad@users.noreply.github.com>
2026-09-10 12:56:12 -0500

    Create start-wisp.bat to start Wisp server

---
+++ start-wisp.bat
@@ -0,0 +1,13 @@
+@echo off
+title Wisp Server
+
+echo ==============================
+echo       Starting Wisp...
+echo ==============================
+echo.
+
+npm start
+
+echo.
+echo Wisp has stopped.
+pause
